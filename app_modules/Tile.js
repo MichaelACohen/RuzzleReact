ecfbf14301d2bedcef58669bdfb60ea7835a06bb
Michael Cohen <michael@cohenhome.net>
2016-07-29 00:34:29 -0700

    cleaned up tile class

--- app_modules/Tile.js
+++ app_modules/Tile.js
@@ -14,16 +14,16 @@ var Tile = React.createClass({
   },
   getStyle: function() {
     var style = [styles.tile, {width: this.getWidth()}];
-    if (this.props.selected) style.push({backgroundColor: 'yellow'});
+    if (this.props.selected) style.push(styles.selected);
     return style;
    },
   render: function() {
     return (
       <TouchableOpacity activeOpacity={0.6} style={this.getStyle()} onPress={this.props.onPress.bind(this)}>
-        <View style={{position: 'absolute', left: 0, right: this.getWidth()/15, top: this.getWidth()/15, alignItems: 'flex-end'}}>
-          <Text style={{color: 'black', fontSize: this.props.fontSize/5}}>{Config.letterValues[this.props.letter]}</Text>
+        <View style={[styles.numContainer, {left: 0, right: this.getWidth()/10, top: this.getWidth()/10}]}>
+          <Text style={[styles.text, {fontSize: this.props.fontSize/5}]}>{Config.letterValues[this.props.letter]}</Text>
         </View>
-        <Text style={{color: 'black', fontWeight: 'bold', fontSize: this.props.fontSize}}>{this.props.letter}</Text>
+        <Text style={[styles.text, styles.letterText, {fontSize: this.props.fontSize}]}>{this.props.letter}</Text>
       </TouchableOpacity>
     );
   }
@@ -34,6 +34,19 @@ var styles = StyleSheet.create({
     backgroundColor: 'blue',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  selected: {
+    backgroundColor: 'yellow'
+  },
+  numContainer: {
+    position: 'absolute',
+    alignItems: 'flex-end'
+  },
+  text: {
+    color: 'black'
+  },
+  letterText: {
+    fontWeight: 'bold'
   }
 });
 
